a8eaec4d83e726c34f86333f4de9417d8517dcf1
DevvAaron <ahuamanve18@ucvvirtual.edu.pe>
2024-08-24 00:59:35 -0500

    avance

---
+++ api_ecommerce/index.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+import path from "path"
+import mongoose from "mongoose"
+//import router from 
+
+// CONEXION A LA BASE DE DATOS
+
+mongoose.Promise = global.Promise;
+const dbURL = "mongodb://localhost:27017/ecommerce_udemy";
+mongoose.connect(
+    dbURL , {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(mongoose => console.log("CONECTASTE CON EXITO A LA BASE DE DATOS"))
+.catch(err => console.log(err));
+
+const app = express();
+app.use(cors());
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+app.use(express.static(path.join(__dirname,'public')));
+
+//app.use('api/',router)
+
+//seteamos el puerto a usar
+app.set('port', process.env.PORT || 3000 );
+
+app.listen(app.get('port'), () => {
+    console.log("EL SERVIDOR SE EJECUTO EN EL PUERTO 3000")
+})
+
