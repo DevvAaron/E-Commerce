d451e92325ef12ee942806cbea974585f9f151b2
DevvAaron <ahuamanve18@ucvvirtual.edu.pe>
2024-08-26 11:55:44 -0500

    Registro Ecommerce Backend

--- ecommerce/src/app/modules/login/login.component.ts
+++ ecommerce/src/app/modules/login/login.component.ts
@@ -16,6 +16,9 @@ export class LoginComponent implements OnInit{
   ) {}
   ngOnInit(): void {
     // console.log(this.authService.user);
+    if (this.authService.user) {
+      this.router.navigate(["/"]);
+    }
   }
 
   login(){
--- ecommerce/src/app/modules/register/register.component.ts
+++ ecommerce/src/app/modules/register/register.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth-profile/_service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
+  email:any ='';
+  name:any ='';
+  surname:any ='';
+  password:any ='';
+  repet_password:any ='';
+
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+  )
+  { }
+
+  ngOnInit(): void {   
+    
+    if (this.authService.user) {
+      this.router.navigate(["/"]);
+    } 
+  }
+
+  registro(){
+    if (!this.email ||
+      !this.name ||
+      !this.surname ||
+      !this.password ||
+      !this.repet_password) {
+      alert("TODOS LOS CAMPOS SON REQUERIDOS");
+    }
+    if (this.password != this.repet_password) {
+      alert("LAS CONSTRASEÑAS TIENEN QUE SER IGUALES");
+    }
+    let data= {
+      email: this.email,
+      name: this.name,
+      surname: this.surname,
+      password: this.password,
+      repet_password: this.repet_password,
+      rol: 'cliente',
+    };
+    this.authService.registro(data).subscribe((resp:any)=>{
+      console.log(resp);
+    });
+  }
 }
